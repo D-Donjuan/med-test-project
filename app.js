58663fe0c84b17b6bac6d418766cfc09bf2d1e77
D-Donjuan <ddonjuan57@gmail.com>
2023-01-23 23:26:05 -0800

    implemented handlebars template engine to render UI

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const express = require('express')
-const dotenv = require('dotenv')
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 
 //Load config
-dotenv.config({path: './config/config.env'})
+dotenv.config({path: './config/config.env'});
 
-const app = express()
-const PORT = process.env.PORT || 5000
+const app = express();
+
+//morgan is a logger that shows any HTTP request that comes in the console
+if(process.env.NODE_ENV === 'development')
+{
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+//changing file extension of template engine from .handlebars -> .hbs
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routes
+app.use('/', require('./routes/index'))
 
 app.listen(
     PORT, 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-)
+);
